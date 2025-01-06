e459c83094ed8aa01baae654562254e50758cc54
sanket kore <sanketkore09@gmail.com>
2025-01-06 15:19:33 +0530

    file addedcommit

---
+++ index.js
@@ -0,0 +1,46 @@
+let boxes=document.querySelectorAll(".box");
+let msgcontainer=document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
+let newbtn=document.querySelector("#new-btn")
+const winpattern=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+let turno=true;
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        console.log(" box is clicked") 
+        if(turno){
+            box.innerText="o";
+            turno=false
+
+        }
+        else{
+            box.innerText="x";
+            turno=true;
+        }
+        box.disabled=true;
+        checkwinner();
+
+    })
+});
+let showwinner=(winner)=>{
+    msg.innerText=`congratulation winner is ${winner}`;
+    msgcontainer.classList.remove("hide");
+    }
+const checkwinner=()=>{
+for(pattern of winpattern)
+{
+let pos1=boxes[pattern[0]].innerText;
+let pos2=boxes[pattern[1]].innerText;
+let pos3=boxes[pattern[2]].innerText;
+if(pos1!=""&&pos2!=""&&pos3!="")
+{
+    if(pos1==pos2&&pos2==pos3)
+    {
+        console.log("winner",pos1)
+        showwinner(pos1);
+
+    }
+   
+}
+
+}
+};
